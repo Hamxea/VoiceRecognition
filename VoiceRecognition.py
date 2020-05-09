09a256c40172ac992ddb4698750119b29badfac4
hamza.mohammed <Gungunbap1>
2020-05-09 21:13:16 +0300

    update swig and pocketsphinx configuration

--- VoiceRecognition.py
+++ VoiceRecognition.py
@@ -3,6 +3,7 @@ import speech_recognition as sr
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
     print("Say something!")
     audio = r.listen(source)
 
